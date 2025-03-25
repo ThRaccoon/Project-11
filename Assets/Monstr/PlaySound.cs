e56bb03a412d598528ba81a6f6f71a7682948a27
presiyan.vasilev <108042195+sporky40@users.noreply.github.com>
2025-03-26 00:46:43 +0200

    111

--- Assets/Monstr/PlaySound.cs
+++ Assets/Monstr/PlaySound.cs
@@ -4,13 +4,17 @@ public class PlaySound : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private AudioSource m_audioSource;
-    void Start()
+    void Awake()
     {
         m_audioSource = GetComponent<AudioSource>();
     }
 
     public void playSound()
     {
-        m_audioSource.Play();
+        if(m_audioSource != null)
+        {
+            m_audioSource.Play();
+        }
+       
     }
 }
